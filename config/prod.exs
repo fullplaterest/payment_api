61541ef2b2fa42d10e378edb6ff92d4b9f0a6b70
igorsousap <igorsousapinto140@gmail.com>
2025-05-16 19:50:16 -0300

    adds formatting

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :payment_api, PaymentApiWeb.Endpoint
-  #cache_static_manifest: "priv/static/cache_manifest.json"
+# cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: PaymentApi.Finch
